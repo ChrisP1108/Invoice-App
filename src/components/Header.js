af0857f89fe5b5c2e78ca5284f8ee950a32edfd8
Chris Paschall <ChrisP1108@gmail.com>
2021-08-03 10:09:28 -0700

    Invoice Title, Filter, And New Button Completion

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,14 +8,14 @@ const Header = () => {
 
     return (
         <div id="header" className="d-flex flex-row flex-xl-column align-items-center justify-content-between">
-            <div className="logo-container centerize">
+            <div className="logo-container f-c">
                 <div onClick={() => reload()}className="logo pointer"></div>
             </div>
-            <div className="centerize flex-row flex-xl-column">
-                <div onClick={() => toggleNightMode()} className="d-flex align-items-center justify-content-center switch-container" >
+            <div className="f-c flex-row flex-xl-column">
+                <div onClick={() => toggleNightMode()} className="f-c switch-container" >
                     <div className={`${nightMode() ? `theme-night` : `theme-day`} theme pointer`}></div>
                 </div>
-                <div className="avatar-container centerize pointer">
+                <div className="avatar-container f-c pointer">
                     <div className="avatar-image"></div>
                 </div>
             </div>
--- src/components/Viewer.js
+++ src/components/Viewer.js
@@ -14,10 +14,24 @@ const Viewer = () => {
     return (
         <div id="viewer" className={nightMode() ? 'night-mode' : 'day-mode'}>
             <div className="viewer-container">
-                <div className="row m-0 viewer-container-filter">
-                    <div className="col-5 m-0 p-0 d-flex flex-column justify-content-around">
+                <div className="row m-0 viewer-container-filter d-flex justify-content-between">
+                    <div className="col-5 ns d-flex flex-column justify-content-around">
                         <h1>Invoices</h1>
                         <h2>{list.length === 0 ? `No invoices` : `${list.length} invoices`}</h2>
+                    </div>
+                    <div className="col-7 ns f-ae pointer">
+                        <h3>Filter</h3>
+                        <div className="filter-container f-c">
+                            <div className="filter-arrow"></div>
+                        </div>
+                        <div className="button-container pointer f-c">
+                            <div className="f-c">
+                                <div className="button-circle f-c">
+                                    <div className="button-icon"></div>
+                                </div>
+                            </div>
+                            <div className="button"><span>New</span></div>
+                        </div>
                     </div>
                 </div>
             </div>
