76a22781a6518434a3c33fae9910ae4d12009116
Chris Paschall <ChrisP1108@gmail.com>
2021-08-23 09:28:19 -0700

    App.json update

--- src/redux/fetchUrl.js
+++ src/redux/fetchUrl.js
@@ -1,6 +1,9 @@
-export const Url = 'http://localhost:5000/invoices';
+// Localhost Server
 
-// Raw Github db.json
+    export const Url = 'http://localhost:5000/invoices';
+
+
+// Raw Github app.json
 
     // 'https://raw.githubusercontent.com/ChrisP1108/Invoice-App/list/db.json';
 
