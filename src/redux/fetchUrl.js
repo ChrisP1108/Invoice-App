b23abeedfff231e769c9ae1b07ce5d32806e5ef0
Chris Paschall <ChrisP1108@gmail.com>
2021-08-23 09:30:44 -0700

    Setup to read App.json for get requests.  Cannot make PUT, POST, OR DELETE REQUESTS

--- src/redux/fetchUrl.js
+++ src/redux/fetchUrl.js
@@ -1,10 +1,8 @@
 // Localhost Server
 
-    export const Url = 'http://localhost:5000/invoices';
+    // export const Url = 'http://localhost:5000/invoices';
 
 
 // Raw Github app.json
 
-    // 'https://raw.githubusercontent.com/ChrisP1108/Invoice-App/list/db.json';
-
-
+    export const Url = 'https://raw.githubusercontent.com/ChrisP1108/Invoice-App/main/app.json';
